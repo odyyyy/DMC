8b99d507da920adfbf381e28e858fb036bbd7af7
odyyyy <iliya.odyvan4ikys@gmail.com>
2024-04-07 14:43:49 +0300

    add readonly_fields in admin

--- survey/admin.py
+++ survey/admin.py
@@ -9,12 +9,14 @@ class SurveyUserResultAdmin(admin.ModelAdmin):
     )
     list_display = ('id', 'car_number', 'average_rating', 'published_datetime')
     search_fields = ('car_number',)
+    readonly_fields = ('average_rating', 'comment')
 
 
 class SurveyAdmin(admin.ModelAdmin):
     list_display = ('car_number_id', 'car_number_text', 'question', 'rating')
     list_filter = ('question',)
     search_fields = ('car_number__car_number',)
+    readonly_fields = ('rating',)
 
     @admin.display(description="Номер автомобиля")
     def car_number_text(self, obj):
@@ -29,6 +31,7 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question', 'question_average_rating')
     list_filter = ('question',)
     search_fields = ('question',)
+    readonly_fields = ('question_average_rating',)
 
 
 admin.site.register(Question, QuestionAdmin)
